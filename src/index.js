b3a15db1496286b9420812531b1448954e7b4dc8
jijek-com <mas.eug@yandex.ru>
2026-04-27 12:46:00 +0500

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const dict = new Map();
+
+  for (let i = 0; i < bracketsConfig.length; i += 1) {
+    dict.set(bracketsConfig[i][0], bracketsConfig[i][1]);
+  }
+
+  const stack = [];
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (
+      dict.has(stack[stack.length - 1]) &&
+      dict.get(stack[stack.length - 1]) === str[i]
+    ) {
+      stack.pop();
+    } else {
+      stack.push(str[i]);
+    }
+  }
+
+  return stack.length === 0;
 };
